2b07f10f9927bd41a9c2f27f3cf1e5c44d631725
xkgary <kiyozxc@hotmail.com>
2015-06-18 23:29:06 +0800

    Ryze

--- Program.cs
+++ Program.cs
@@ -251,7 +251,7 @@ namespace AutoLevelup
                 }
             }
             else if (Player.BaseSkinName == "Rumble") abilitySequence = new int[] { 1, 3, 2, 1, 1, 4, 1, 3, 1, 3, 4, 3, 3, 2, 2, 4, 2, 2 };
-            else if (Player.BaseSkinName == "Ryze") abilitySequence = new int[] { 1, 3, 2, 1, 1, 4, 1, 2, 1, 2, 4, 2, 2, 3, 3, 4, 3, 3 };
+            else if (Player.BaseSkinName == "Ryze") abilitySequence = new int[] { 3, 2, 1, 2, 2, 4, 1, 2, 2, 1, 4, 1, 1, 3, 3, 4, 3, 3 };
             else if (Player.BaseSkinName == "Sejuani") abilitySequence = new int[] { 2, 3, 1, 2, 2, 4, 2, 1, 2, 3, 4, 3, 3, 3, 1, 4, 1, 1 };
             else if (Player.BaseSkinName == "Shaco")
             {
